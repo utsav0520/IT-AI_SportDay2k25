ade9a987b9487871a5acaca576bcb138a1a646f7
Utsav Vachhani (uv) <uvachhani03@gmail.com>
2025-03-01 03:02:20 +0000

    Pat changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from './Layout'
 import Home from './components/Home'
 import Bandminton from './components/Pages/Bandminton'
-import Cricket  from './components/Pages/Cricket '
+import Cricket from './components/Pages/Cricket'
 import Chess from './components/Pages/Chess'
 import TugofWar from './components/Pages/TugofWar'
 import Volleyball from './components/Pages/Volleyball'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout/>}>
-      <Route path="/" element={<Home />} />
-      <Route path="home" element={<Home />} />
-      <Route path="bandminton" element={<Bandminton />} />
-      <Route path="cricket " element={<Cricket  />} />
-      <Route path="tugofwar" element={<TugofWar />} />
-      <Route path="chess" element={<Chess />} />
-      <Route path="volleyball" element={<Volleyball />} />
-    </Route>
-  )
-)
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="bandminton" element={<Bandminton />} />
+          <Route path="cricket" element={<Cricket />} />
+          <Route path="tugofwar" element={<TugofWar />} />
+          <Route path="chess" element={<Chess />} />
+          <Route path="volleyball" element={<Volleyball />} />
+        </Routes>
+      </Layout>
+    </Router>
+  </StrictMode>
 )
