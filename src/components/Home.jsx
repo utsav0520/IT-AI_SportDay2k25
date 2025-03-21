c41372ca48b07757116664a2e198dbb7af137aba
Utsav Vachhani (uv) <uvachhani03@gmail.com>
2025-03-21 12:06:05 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className='cointaner animated-background-home'>
     <h1 className='text-home'>Let's Participate : </h1>
-    <button><NavLink to='/bandminton'>BadMiniton</NavLink></button>
+    <button><NavLink to='/bandminton'>BadMinton</NavLink></button>
     <button><NavLink to='/chess'> Chess</NavLink></button>
     <button><NavLink to='/cricket'>Cricket </NavLink></button>
     <button><NavLink to='/tugofwar'>Tug of War </NavLink></button>
